5e7c3b6ec2ac8901e5bf15c7e4a7bf0bb5797f0b
Ironboy <ironboy@users.noreply.github.com>
2022-04-01 14:01:33 +0200

    corrected the unit tests

--- test/mock.test.js
+++ test/mock.test.js
@@ -7,7 +7,7 @@ const { expect } = require('@jest/globals');
 
 describe('A test suite', () => {
 
-  test("A fake test", () => {
+  test('A fake test', () => {
 
     expect(3).toBe(3); // failing on purpose
 
